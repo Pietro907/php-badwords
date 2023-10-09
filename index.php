60f5c85225fcdfaacd3b6fd4cd0cdf2b32cf5b62
Pietro907 <pietrop.puri@gmail.com>
2023-10-09 17:17:56 +0200

    add input censured word

--- index.php
+++ index.php
@@ -7,16 +7,31 @@
     <title>PHP Badwords</title>
 </head>
 
+<style>
+    .container {
+        width: 500px;
+        margin: 8rem auto 0;
+        text-align: start;
+    }
+</style>
+
 <body>
 
-    <!-- method="GET" action="index.php" -->
-    <form method="GET" action="script.php">
-        <input type="text" name="text" id="text" placeholder="Scrivi qui...">
-        <input type="text" name="censured" id="censured" placeholder="Scrivi qui e ti censuro...">
-        <button type="submit">Invia</button>
-    </form>
+    <div class="container">
 
-    
+        <h1>PHP BadWord</h1>
+
+        <!-- method="GET" action="index.php" -->
+        <form method="GET" action="script.php">
+            <input type="text" name="text" id="text" placeholder="Scrivi qui...">
+            <input type="text" name="censured" id="censured" placeholder="Scrivi qui e ti censuro...">
+            <button type="submit">Invia</button>
+        </form>
+
+
+    </div>
+
+
 
 
 
--- script.php
+++ script.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 
 $text = $_GET['text'];
 $censured = $_GET['censured'];
+
 
 
 
@@ -11,11 +12,39 @@ $censured = $_GET['censured'];
 <!DOCTYPE html>
 <html lang="en">
 
-<body>
+<style>
 
-    <p>Text: <?php echo $text; ?> <?php echo strlen($text); ?></p>
-    <p>Text censured: <?php echo $censured ?> <?php echo strlen($censured) ?></p>
-    
+.container{
+    width: 500px;
+    margin: 8rem auto 0;
+    text-align: start;
+}
+
+</style>
+
+<body>
+    <div class="container">
+
+        <p>Text: <?php echo $text; ?> <?php echo strlen($text); ?></p>
+
+        <p>Text censured:
+
+            <?php
+
+            if (strlen($censured)) {
+                echo $censured;
+            } else {
+                echo "***";
+            }; 
+            
+            ?>
+
+        </p>
+
+
+    </div>
+
+
 
 </body>
 
