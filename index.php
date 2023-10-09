fcad5c4dbbd3e6e32295cf34c97c8d5257ca0f34
Pietro907 <pietrop.puri@gmail.com>
2023-10-09 15:06:59 +0200

    add initial page

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<?php 
+
+
+
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+
+<!-- method="GET" action="index.php" -->
+    <form>
+        <input type="text" name="text" id="text"  placeholder="Scrivi qui..." >
+        <button type="submit">Invia</button>
+    </form>
+
+
+
+    
+</body>
+</html>
